bd40c71311b4c1baa1e43f0d589341c7987eeafa
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-05-26 21:03:00 +0330

    feat: improve token refresh logic in httpService

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -1,8 +1,30 @@
 import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api";
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true
+      try {
+        const { data } = await axios.get(`${BASE_URL}/user/refresh-token`, {
+          withCredentials: true,
+        });
+        if (data) return axiosInstance(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
 
 const http = {
   get: axiosInstance.get,
@@ -13,4 +35,3 @@ const http = {
 };
 
 export default http;
-
